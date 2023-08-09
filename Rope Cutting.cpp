65fc5568298f0a32b33e3f8b56fb32f7a2f1a70e
MALIREDDY TRINAY REDDY <109901688+trinayreddy4@users.noreply.github.com>
2023-08-09 20:45:39 +0530

    Add files via upload

---
+++ Rope Cutting.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int  ropeCutting(int n,int a,int b,int c)
+{
+	
+	if(n==0)
+	return 0;
+	
+	if(n<0)
+	return -1;
+	
+	int res=max(ropeCutting(n-a,a,b,c),ropeCutting(n-b,a,b,c),ropeCutting(n-c,a,b,c));
+	if(res==-1)
+	return -1;
+	
+	return res+1;
+}
+int main()
+{
+	int n,a,b,c;
+	cin>>n>>a>>b>>c;
+	
+	cout<<ropeCutting(n,a,b,c)<<" are the maximum pieces that can be cut from the rope";
+}
